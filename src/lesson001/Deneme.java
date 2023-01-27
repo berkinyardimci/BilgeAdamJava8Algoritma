456791e8d6e280b377ed4f15e663f0a6ea6cf1b2
Berkin YARDIMCI (BilgeAdam) <berkin.yardimci@bilgeadam.com>
2023-01-27 16:45:58 +0300

    first commit

---
+++ src/lesson001/Deneme.java
@@ -0,0 +1,18 @@
+package lesson001;
+
+public class Deneme {
+
+	public static void main(String[] args) {
+		System.out.println("Deneme");
+		
+		//ctrl + f11
+		//BilgeAdamAlgoritma Reo oluşturalım
+		//bu rojeyi githuba atlaım
+		
+		
+		//git add .
+		//git commit -m "2.değişiklikler yapıldı"
+		//git push
+		
+	}
+}
