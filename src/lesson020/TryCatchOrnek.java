70ba00e158f667e233ca16231feeff6a51451cbc
berkinyardimci <berkin.yardimci@bilgeadam.com>
2023-03-06 15:59:31 +0300

    tryCatch

--- src/lesson020/TryCatchOrnek.java
+++ src/lesson020/TryCatchOrnek.java
@@ -5,33 +5,47 @@ import java.util.Scanner;
 
 public class TryCatchOrnek {
 	public static void main(String[] args) {
-		int bolum1 = bolme();
-		System.out.println(bolum1);
+		double bolum1;
+		try {
+			bolum1 = bolme();
+			System.out.println(bolum1);
+		} catch (IllegalArgumentException e) {
+			System.out.println(Constant.ILLEGAL_ARGUMENT);
+		}
+		
 	}
+	
+	//14:55
+	
 
 	// bir tane bölme methodu yazalım kullanıcıdan 2 tane sayı alcaz
 	// ve bölümü return dicez bunu yaparken gerekli hataları karşı önlem alalım
 
 	// kulanıcıdı doğru sonuc girene kadar kullanıcdan girdi alalım
 
-	public static int bolme() {
+	public static double bolme() throws IllegalArgumentException {
 		boolean kontrol = true;
-		int bolum = 0;
+		double bolum = 0;
+		double sayi1 =0;
+		double sayi2 = 0;
 		while (kontrol) {
 			try {
 				Scanner scanner = new Scanner(System.in);
 				System.out.println("1. Sayi");
-				int sayi1 = scanner.nextInt();
+				 sayi1 = scanner.nextDouble();
 				System.out.println("2. Sayi");
-				int sayi2 = scanner.nextInt();
+				sayi2 = scanner.nextDouble();
 				bolum = sayi1 / sayi2;
 				kontrol = false;
 			} catch (ArithmeticException e) {
-				System.out.println("Sayi 0 a bölünemez");
+				System.out.println(Constant.NUMBER_EQUALS_ZERO);
 			}  catch (InputMismatchException e) {
-				System.out.println("lütfen sayi giriniz");
+				System.out.println(Constant.MIS_MATCH_EXCEPTION);
 			}catch (Exception e) {
-				System.out.println("Beklenmedik bir hata meydana geldi");
+				System.out.println(Constant.UNEXPECTED_EXCEPTION);
+			}
+			if(sayi2 ==1) {
+				 throw new IllegalArgumentException(Constant.NUMBER_EQUALS_ZERO);
 			}
 		}
 		return bolum;
